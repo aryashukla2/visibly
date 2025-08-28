3e6f0d5e70e5d6f939a15a4d5b2f9970b23c04d7
aryashukla1 <ashukla326@student.fuhsd.org>
2025-08-27 21:30:50 -0400

    connected to mongo

---
+++ backend/app/db.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_URL = os.getenv("MONGO_URL")
+client = MongoClient(DB_URL)
+db = client["visiblydb"]
+users_collection = db["users"]
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from app.db import users_collection
+from app.schemas import User
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Backend is running in Docker"}
+
+@app.post("/users/")
+def add_user(user: User):
+    result = users_collection.insert_one(user.dict())
+    return {"inserted_id": str(result.inserted_id)}
